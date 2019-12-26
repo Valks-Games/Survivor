c79cc83ad404196e09be01f98f58cafa47b61fa5
valkyrienyanko <sebastianbelle074@gmail.com>
2019-12-25 21:27:21 -0600

    UI World Space Experimental

---
+++ Assets/PanelBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelBase : MonoBehaviour
+{
+    private RectTransform _rt;
+    private GameObject _baseOne;
+
+    void Start()
+    {
+        _rt = GetComponent<RectTransform>();
+        _baseOne = GameObject.Find("Base 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rt.anchoredPosition = _baseOne.transform.position;
+    }
+}
