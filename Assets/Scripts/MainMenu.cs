e1aa72dac3dd36bfab9fb9339c75b20847e134f2
UZ9 <chessboy5@icloud.com>
2019-12-26 19:17:36 -0600

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else 
+        Application.Quit();
+        #endif
+    }
+
+
+}
