982682be6d917bc09e1f1c6fd4cba4a2bc55092f
valkyrienyanko <sebastianbelle074@gmail.com>
2020-01-15 22:39:54 -0600

    Shader Learning

---
+++ Assets/Workspace/Shaders/FogOfWar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        float speed = 100f;
+        meshRenderer.material.SetTextureOffset("Main Texture", new Vector2(horz * speed, vert * speed));
+    }
+}
---
+++ Assets/Workspace/Shaders/NewUnlitShader.shader
@@ -0,0 +1,16 @@
+﻿Shader "Unlit/NewUnlitShader"
+{
+    Properties
+    {
+        _Color ("Main Color", Color) = (1, 0.5, 0.5, 1)
+    }
+    SubShader
+    {
+        Pass {
+            Material {
+                Diffuse [_Color]
+            }
+            Lighting On
+        }
+    }
+}
