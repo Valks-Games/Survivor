3994b79eb0e83746f88e66860eca9ae1b5aff8f8
valkyrienyanko <sebastianbelle074@gmail.com>
2020-01-16 15:35:58 -0600

    Fade Transition

---
+++ Assets/Workspace/Scripts/UI/PanelFadeTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelFadeTransition : MonoBehaviour
+{
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        image.CrossFadeAlpha(0f, 1f, false);
+    }
+}
